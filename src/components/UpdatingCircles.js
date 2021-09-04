ecfff74a769afeec3a94c4150d6212f7ae0a6783
StjepanDev <grunf69@yahoo.com>
2021-09-04 20:50:04 +0200

    d

---
+++ src/components/UpdatingCircles.js
@@ -0,0 +1,35 @@
+import React, { useRef, useEffect, useState } from 'react';
+
+import { select } from 'd3';
+
+function UpdatingCircles() {
+  const svgRef = useRef();
+  const [data, setData] = useState([26, 75, 45, 60, 37]);
+
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    svg
+      .selectAll('circle')
+      .data(data)
+      .join('circle')
+      .attr('r', (d) => d)
+      .attr('cx', (d) => d * 2)
+      .attr('cy', (d) => d * 1)
+      .attr('stroke', 'red');
+  }, [data]);
+
+  return (
+    <>
+      <svg ref={svgRef}></svg>
+      <br />
+      <button onClick={() => setData(data.map((value) => value + 8))}>
+        Update Data
+      </button>
+      <button onClick={() => setData(data.filter((value) => value < 45))}>
+        Filter Data
+      </button>
+    </>
+  );
+}
+
+export default UpdatingCircles;
